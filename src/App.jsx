1181c33e74aa6b26d446a45bb3aea6ce4cc46c8e
amanst1 <seifeamanuel@gmail.com>
2024-02-18 19:39:12 +0300

    add shuffle function, edit appearance

--- src/App.jsx
+++ src/App.jsx
@@ -11,18 +11,38 @@ function App() {
 
   const images = []
 
+  function shuffle(array) {
+    let currentIndex = array.length,  randomIndex;
+  
+    // While there remain elements to shuffle.
+    while (currentIndex > 0) {
+  
+      // Pick a remaining element.
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+  
+      // And swap it with the current element.
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex], array[currentIndex]];
+    }
+  
+    return array;
+  }
+
   const resetGame = () => {
     setScore(0)
     setStatus('')
     setClicked([])
+    window.location.reload()
   }
 
   const handleClick = (componentId) => {
       
       if(!clicked.includes(componentId)) {
-        setClicked( prev => [...prev, componentId] )
-        setScore(score => score + 1)
-        setBestScore(bestScore => bestScore + 1)
+        setClicked( prev => [...prev, componentId] );
+        setScore(score => score + 1);
+        (score >= bestScore) ? setBestScore(bestScore => bestScore + 1) : '';
+        shuffle(pokemons);
         if(score === 11) {
           setStatus('victory')
         }
@@ -30,14 +50,16 @@ function App() {
         setStatus('loss')
       }
   }
+
   
-  //
+  // Populate array of Image components from pokemon array
   pokemons.map(pokemon => {
     images.push(<ImageComponent 
       key={pokemon.id}  id={pokemon.id} url={pokemon.url}
       onClick={(id) => handleClick(id)} /> )
   })
   
+
   return (
     <>
       <h1> Did I click on that...?</h1>
@@ -45,7 +67,7 @@ function App() {
       <h4>Highscore: { bestScore } </h4>
 
       {status === '' &&
-        (<div style={{display: 'flex', 'flex-flow': 'row wrap'}}>
+        (<div style={{display: 'flex', 'flex-flow': 'row wrap', 'justifyContent':'center'}}>
           
             {images}
           
